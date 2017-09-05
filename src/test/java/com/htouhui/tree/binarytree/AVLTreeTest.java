e8c5040193b106d3e56e49e7d3418fcf3f763c29
= <nan.wang@htouhui.com>
2017-09-05 18:42:00 +0800

    UPDATE AVL TREE TEST

--- src/test/java/com/htouhui/tree/binarytree/AVLTreeTest.java
+++ src/test/java/com/htouhui/tree/binarytree/AVLTreeTest.java
@@ -1,5 +1,6 @@
 package com.htouhui.tree.binarytree;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,35 +9,83 @@ import org.junit.Test;
  */
 public class AVLTreeTest {
     private static int arr[] = {3, 2, 1, 4, 5, 6, 7, 16, 15, 14, 13, 12, 11, 10, 8, 9};
+    private AVLTree<Integer> tree;
 
-    @Test
-    public void test() {
-        int i;
-        AVLTree<Integer> tree = new AVLTree<Integer>();
+    @Before
+    public void before() {
+        tree = new AVLTree<>();
 
         System.out.printf("== 依次添加: ");
-        for (i = 0; i < arr.length; i++) {
-            System.out.printf("%d ", arr[i]);
-            tree.insert(arr[i]);
+        for (int i : arr) {
+            System.out.printf("%d ", i * 2);
+            tree.insert(i * 2);
         }
+    }
 
+    @Test
+    public void testPreOrder() {
         System.out.printf("\n== 前序遍历: ");
         tree.preOrder();
+    }
 
+    @Test
+    public void testInOrder() {
         System.out.printf("\n== 中序遍历: ");
         tree.inOrder();
+    }
 
+    @Test
+    public void testPostOrder() {
         System.out.printf("\n== 后序遍历: ");
         tree.postOrder();
-        System.out.printf("\n");
+    }
 
+    @Test
+    public void testInsert() {
+        System.out.println();
+        System.out.println("Height : " + tree.height());
+        tree.print();
+
+        System.out.printf("== 依次添加: ");
+        for (int i : arr) {
+            AVLTree<Integer>.AVLTreeNode<Integer> node = tree.search(i);
+            if (node == null) {
+                System.out.printf("%d ", i);
+                tree.insert(i);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Height : " + tree.height());
+        tree.print();
+
+        for (int i = 0; i < 100; i++) {
+            AVLTree<Integer>.AVLTreeNode<Integer> node = tree.search(i);
+            if (node == null) {
+                System.out.printf("%d ", i);
+                tree.insert(i);
+            }
+        }
+
+
+        System.out.println();
+        System.out.println("Height : " + tree.height());
+        tree.print();
+    }
+
+
+    @Test
+    public void printInfo() {
         System.out.printf("== 高度: %d\n", tree.height());
         System.out.printf("== 最小值: %d\n", tree.minimum());
         System.out.printf("== 最大值: %d\n", tree.maximum());
         System.out.printf("== 树的详细信息: \n");
         tree.print();
+    }
 
-        i = 8;
+    @Test
+    public void test() {
+        int i = 8;
         System.out.printf("\n== 删除根节点: %d", i);
         tree.remove(i);
 
